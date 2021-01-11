d49369176e4aa9d08d62363510f4d2cff6114612
Jędrzej <43277806+BasileusRomanum@users.noreply.github.com>
2021-01-11 06:17:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import re
 import os
 import logging
+import mysql.connector
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
@@ -9,7 +10,12 @@ SITE_ADDRESS = 'https://showup.tv'
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
-
+'''
+mydb = mysql.connector.connect(
+        host='sql7.freesqldatabase.com',
+        user='sql7385402',
+        password='fLGXMRLKyp')
+'''
 
 while True:
         try:
@@ -31,21 +37,24 @@ while True:
                 driver.refresh()
                 #Tutaj do dodania jest import i obróbka .csv z wynikami. Czyli splitlines() i potem w pętelce split(";") i wsio
                 #.csv ma mieć strukturę 'Nick';ilość widzów;ilość widzów;ilość widzów(...).
-                #Poza pierwszą linijką, która jest kontrolona i jednocześnie przechowuje czas pobrania danych ze strony.
-                #Taka transpozycja ułatwi mi obróbkę tutaj, a transpozycję do klasycznego wyglądu, żeby to wyrzygać na wykres
-                #zrobi się pandą w skrypcie, które będzie generował htmla z wykresem. Docelowo modyfikowalnym w czasie rzeczywistym.
-                #Kek. Generalnie to powinienem to wrzucać od razu do bazy danych. Ale jestem leniwy. I nie wiem za bardzo jeszcze jak to zrobić.
-                #Musiałbym pogrzebać w metodach dataframe.
                 #Ale to już powinno być w JS, a nie pajtonku. HA, TAKI CHUJ. I TAK JEST W PYTHONIE
                 #Otwieramy .csv z nazwamia modelek i liczbą widzów. Potem dodajemy czas, kiedy pobrano wsio
+                #Docelowo przepiszę kod na korzystanie z jakieś .db
                 with open('dane.csv', 'r') as fileStart:
                     results = [x.split(";") for x in fileStart.read().splitlines()]
                 results[0].append(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 
-                
+                #Wyciągamy liczbę widzów
                 viewersNumber = re.findall("i ([0-9]+[ ])\w+", driver.page_source)[0][:-1]
+                #Wyciągamy liczbę transmisji
                 transmissionsNumber = re.findall("<h4>([0-9]+[ ])\w+", driver.page_source)[0][:-1]
+                #Wyciągamy listę z nickami modelek
+                modelNameList = re.findall('<a href="/([^"]*)', driver.page_source)[8:-5]
+                #Wyciągamy listę z liczbami widzów poszczególnych modelek. Są indeksowane 1:1 z listą wyżej.
+                modelViewersList = re.findall('">([0-9]+)', driver.page_source)
 
+                results[1].append(str(transmissionsNumber))
+                results[2].append(str(viewersNumber))
                 results[3].append(str(round(int(viewersNumber)/int(transmissionsNumber))))
 
 
@@ -55,8 +64,7 @@ while True:
             #Iterujemy przez obiekty w htmlu, które tyczą się pojedyńczych streamów
             #A gówno. Wyszukujemy wszystko regexem. Śmierć burżuazyjnej iteracji.
             #Regexujemy listę nicków i liczbę widzów. Z racji tego, że indeksy obu odpowiadają sobie, to nie ma żadnego problemu.
-                modelNameList = re.findall('<a href="/([^"]*)', driver.page_source)[8:-5]
-                modelViewersList = re.findall('">([0-9]+)', driver.page_source)
+
             #Jeśli jest już, to dodajemy liczbę widzów do odpowiedniej babki
             #Jeśli nie ma, to dodajemy modelkę i liczbę widzów.
                 knownModelsList = [results[x][0] for x in range(len(results))]
@@ -67,7 +75,10 @@ while True:
                     else:
                         logger.info(f"{time.asctime(time.localtime())} Adding {modelName} to the database")
                         toAdd = []
-                        toAdd.append(modelName + '0'*(len(results[0]) - 2) + modelViewersList[modelNameList.index(modelName)])
+                        toAdd.append(modelName)
+                        [toAdd.append('0') for x in range((len(results[0]) - 2))]
+                        toAdd.append(modelViewersList[modelNameList.index(modelName)])
+                        #toAdd.append(modelName + '0'*(len(results[0]) - 2) + modelViewersList[modelNameList.index(modelName)])
                         results.insert(-1, (toAdd))
             #Pamiętaj programisto młody, zawsze zeruj swe metody.
             #Wiem, że to zmienne, ale zmienne się nie rymujo.
@@ -81,8 +92,9 @@ while True:
                 for subListLength in range(len(results)):
                     while len(results[0]) > len(results[subListLength]):
                         results[subListLength].append('0')
+#Sklejanie arrayów do zrzucenia do pliku. I kasowanie ostatniego \n, bo potem .csv interpretuje to jako pustą linię i inne rzeczy się jebiom.
+                toSave = [(";".join(x) + "\n") for x in results][:-2]
 
-                toSave = [(";".join(x) + "\n") for x in results]
                 localTime = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
                 logger.info(f"{localTime} Waiting for the next cycle")
 
